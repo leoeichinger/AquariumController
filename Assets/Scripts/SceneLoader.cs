cd2c7b7624e56975bc643c2e81bccd1eb201f68e
leoeichinger <eichinger.l@gmx.at>
2021-05-01 22:57:45 +0200

    addet Temperature Scene with interactiv and realtime temp visualisation

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadHome()
+    {
+        SceneManager.LoadScene("Home");
+    }
+    public void LoadTemp()
+    {
+        SceneManager.LoadScene("Temperature");
+    }
+}
---
+++ Assets/Scripts/TempManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TempManager : MonoBehaviour
+{
+    float oldTemp;
+    public float newTemp;
+    int correction = 953;
+    public Transform ht;
+
+    void Start() 
+    {
+        InvokeRepeating("SaveOldTemp",0,0.1f);
+        InvokeRepeating("CheckForChange",0.2f,0.2f);
+    }
+
+    void SaveOldTemp()
+    {
+        oldTemp = newTemp;
+    }
+
+    void CheckForChange()
+    {
+
+        if (oldTemp != newTemp)
+        {   
+            newTemp *= 35.2f;
+            ht.position = new Vector3(540, correction + newTemp, 0);
+        }
+    }
+}
